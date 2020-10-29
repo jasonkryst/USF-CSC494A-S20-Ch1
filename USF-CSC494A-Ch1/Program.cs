258bda1fcc14a8609922dbb9cccc5404cb5379c8
Jason Kryst <jasonkryst@gmail.com>
2020-10-29 17:24:02 -0500

    Add project files.

---
+++ USF-CSC494A-Ch1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace USF_CSC494A_Ch1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
